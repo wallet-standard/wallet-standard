c00639b975ea5ecfa3cfa392e0ca038d41265f4d
Jordan Sexton <jordan@jordansexton.com>
2022-07-26 23:12:44 -0500

    restructure and fix initialize function

--- packages/core/standard/src/initialize.ts
+++ packages/core/standard/src/initialize.ts
@@ -11,44 +11,49 @@ import {
 declare const window: WalletsWindow<any>;
 
 export function initialize<Account extends WalletAccount>(): Wallets<Account> {
-    let commands: Wallets<Account> = (window.wallets = window.wallets || []);
+    if (typeof window === 'undefined') return [];
 
-    if (Array.isArray(commands)) {
-        const wallets: Wallet<Account>[] = [];
-        const listeners: { [E in WalletsEventNames<Account>]?: WalletsEvents<Account>[E][] } = {};
+    const commands = (window.wallets = window.wallets || []);
+    // If it's already initialized, just return it
+    if (!Array.isArray(commands)) return commands;
 
-        function push(...commands: WalletsCommand<Account>[]): void {
-            for (const command of commands) {
-                switch (command.method) {
-                    case 'get':
-                        {
-                            const { callback } = command;
-                            callback(wallets);
-                        }
-                        break;
-                    case 'register':
-                        {
-                            const { wallets: newWallets } = command;
-                            wallets.push(...newWallets);
-                            listeners['register']?.forEach((listener) => listener(...newWallets));
-                        }
-                        break;
-                    case 'on':
-                        {
-                            const { event, listener: newListener, callback } = command;
-                            listeners[event]?.push(newListener) || (listeners[event] = [newListener]);
-                            callback(function (): void {
-                                listeners[event] = listeners[event]?.filter((listener) => newListener !== listener);
-                            });
-                        }
-                        break;
-                }
+    const registered: Wallet<Account>[] = [];
+    const listeners: { [E in WalletsEventNames<Account>]?: WalletsEvents<Account>[E][] } = {};
+    const wallets = (window.wallets = { push });
+
+    push(...commands);
+
+    function push(...commands: WalletsCommand<Account>[]): void {
+        for (const command of commands) {
+            switch (command.method) {
+                case 'get':
+                    {
+                        const { callback } = command;
+                        // Return a copy so the original can't be referenced or mutated.
+                        callback([...registered]);
+                    }
+                    break;
+                case 'register':
+                    {
+                        const { wallets } = command;
+                        registered.push(...wallets);
+                        listeners['register']?.forEach((listener) => listener(...wallets));
+                    }
+                    break;
+                case 'on':
+                    {
+                        const { event, listener, callback } = command;
+                        listeners[event]?.push(listener) || (listeners[event] = [listener]);
+                        callback(function off(): void {
+                            listeners[event] = listeners[event]?.filter(
+                                (existingListener) => listener !== existingListener
+                            );
+                        });
+                    }
+                    break;
             }
         }
-
-        push(...commands);
-        commands = window.wallets = { push };
     }
 
-    return commands;
+    return wallets;
 }
