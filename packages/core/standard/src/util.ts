1a0bc929842de529aa3cc2f9eadd6c83ea8967fd
Jordan Sexton <jordan@jordansexton.com>
2022-07-29 19:37:55 -0500

    add method to concatenate Uint8Arrays

--- packages/core/standard/src/util.ts
+++ packages/core/standard/src/util.ts
@@ -11,6 +11,19 @@ export function bytesEqual(a: Bytes, b: Bytes): boolean {
 }
 
 /** TODO: docs */
+export function concatBytes(first: Bytes, ...others: Bytes[]): Bytes {
+    const length = others.reduce((length, bytes) => length + bytes.length, first.length);
+    const bytes = new Uint8Array(length);
+
+    bytes.set(first, 0);
+    for (const other of others) {
+        bytes.set(other, bytes.length);
+    }
+
+    return bytes;
+}
+
+/** TODO: docs */
 export function pick<T, K extends keyof T>(object: T, ...keys: K[]): Pick<T, K> {
     const picked = {} as Pick<T, K>;
     for (const key of keys) {
