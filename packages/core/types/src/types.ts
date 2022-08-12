571e3a8289dd0e32ac07e912574f9a0609101e48
Jordan Sexton <jordan@jordansexton.com>
2022-08-12 15:03:13 -0500

    add types for tuples

--- packages/core/types/src/types.ts
+++ packages/core/types/src/types.ts
@@ -7,6 +7,43 @@ export type PropertyNames<T> = {
 }[keyof T];
 
 /** TODO: docs */
+export type Properties<T> = Pick<T, PropertyNames<T>>;
+
+/** TODO: docs */
 export type MethodNames<T> = {
     [K in keyof T]: T[K] extends (...args: any) => any ? K : never;
 }[keyof T];
+
+/** TODO: docs */
+export type Methods<T> = Pick<T, MethodNames<T>>;
+
+/** TODO: docs */
+export type DistributiveOmit<T, K extends keyof T> = T extends any ? Omit<T, K> : never;
+
+/** TODO: docs */
+export type Tuple<T extends unknown[]> = [...T];
+
+/** TODO: docs */
+export type TupleOmit<T extends any[], K extends PropertyKey> = {
+    [P in keyof T]: Omit<T[P], K>;
+};
+
+/** TODO: docs */
+export type TuplePick<T extends any[], K extends PropertyKey> = {
+    [P in keyof T]: Pick<T[P], K>;
+};
+
+/** TODO: docs */
+export type MapTuple<Input extends unknown[], Value, Output extends unknown[] = []> = Input extends []
+    ? []
+    : Input extends [unknown]
+    ? [...Output, Value]
+    : Input extends [unknown, ...infer Tail]
+    ? MapTuple<[...Tail], Value, [...Output, Value]>
+    : Readonly<Output>;
+
+/** TODO: docs */
+export type AsyncMapFunction<Arg, Return> = <Args extends Arg[]>(...args: Args) => Promise<MapTuple<Args, Return>>;
+
+/** TODO: docs */
+export type MapFunction<Arg, Return> = <Args extends Arg[]>(...args: Args) => MapTuple<Args, Return>;
