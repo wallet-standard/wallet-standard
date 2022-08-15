8915fa2f539c0b405824de9c4d1edd66998b3526
Jordan Sexton <jordan@jordansexton.com>
2022-08-14 23:23:34 -0500

    export types for chains

--- packages/core/util/src/constants.ts
+++ packages/core/util/src/constants.ts
@@ -10,8 +10,18 @@ export const CHAIN_SOLANA_TESTNET = 'solana:testnet';
 /** Solana Localnet, e.g. http://localhost:8899 */
 export const CHAIN_SOLANA_LOCALNET = 'solana:localnet';
 
+/** TODO: docs */
+export type SolanaChain =
+    | typeof CHAIN_SOLANA_MAINNET
+    | typeof CHAIN_SOLANA_DEVNET
+    | typeof CHAIN_SOLANA_TESTNET
+    | typeof CHAIN_SOLANA_LOCALNET;
+
 /** Ethereum (mainnet) */
 export const CHAIN_ETHEREUM = 'ethereum:1';
+
+/** TODO: docs */
+export type EthereumChain = typeof CHAIN_ETHEREUM;
 
 /**
  * Default encryption algorithm in NaCl.
