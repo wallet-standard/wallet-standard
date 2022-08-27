78fca27b7ff48f6a6c1ddc23abab269fe5784c1f
Jordan Sexton <jordan@jordansexton.com>
2022-08-27 13:39:53 -0500

    don't export feature union

--- packages/core/features/src/index.ts
+++ packages/core/features/src/index.ts
@@ -1,25 +1,6 @@
-import type { UnionToIntersection } from '@wallet-standard/types';
-import type { DecryptFeature } from './decrypt.js';
-import type { EncryptFeature } from './encrypt.js';
-import type { SignAndSendTransactionFeature } from './signAndSendTransaction.js';
-import type { SignMessageFeature } from './signMessage.js';
-import type { SignTransactionFeature } from './signTransaction.js';
-import type { SolanaFeature } from './solana.js';
-
 export * from './decrypt.js';
 export * from './encrypt.js';
 export * from './signAndSendTransaction.js';
 export * from './signMessage.js';
 export * from './signTransaction.js';
 export * from './solana.js';
-
-/** TODO: docs */
-export type Feature =
-    | SignTransactionFeature
-    | SignAndSendTransactionFeature
-    | SignMessageFeature
-    | EncryptFeature
-    | DecryptFeature
-    | SolanaFeature;
-
-export type FeatureName = keyof UnionToIntersection<Feature>;
