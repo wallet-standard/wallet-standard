6f9495c9fc6337b609b81c526bb6ee32e5fbebe2
Matias Olivera <moliverafreire@gmail.com>
2022-08-28 17:22:16 -0600

    feat: establish channel between background and content

--- packages/example/extension/src/background/index.ts
+++ packages/example/extension/src/background/index.ts
@@ -1,8 +1,17 @@
+import { CONTENT_PORT_NAME, createChannel, createPortTransport, serializeMessage } from '../messages';
 import { generateMnemonic } from './wallet';
 
 chrome.runtime.onInstalled.addListener((details) => {
     if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
         const mnemonic = generateMnemonic();
         chrome.storage.local.set({ mnemonic });
+    }
+});
+
+chrome.runtime.onConnect.addListener((port) => {
+    if (port.name === CONTENT_PORT_NAME) {
+        const transport = createPortTransport(port);
+
+        const channel = createChannel(transport);
     }
 });
--- packages/example/extension/src/content/window.ts
+++ packages/example/extension/src/content/window.ts
@@ -1,9 +1,17 @@
 import type { WalletsWindow } from '@wallet-standard/standard';
 
+import type { Channel } from '../messages';
+import { createChannel, createWindowTransport } from '../messages';
 import type { MultiChainWalletAccount } from './multiChainWallet';
 import { MultiChainWallet } from './multiChainWallet';
 
-declare const window: WalletsWindow<MultiChainWalletAccount>;
+declare const window: WalletsWindow<MultiChainWalletAccount> & {
+    _channel: Channel;
+};
+
+const transport = createWindowTransport(window);
+const channel = createChannel(transport);
+window._channel = channel;
 
 function register(): void {
     window.navigator.wallets = window.navigator.wallets || [];
