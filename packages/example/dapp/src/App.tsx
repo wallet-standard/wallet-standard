a7101f0eb9c749e2ed1e3817066b25fc30804dc1
Matias Olivera <moliverafreire@gmail.com>
2022-09-06 10:57:09 -0400

    feat: add Connect page

--- packages/example/dapp/src/App.tsx
+++ packages/example/dapp/src/App.tsx
@@ -2,12 +2,14 @@ import type { FC } from 'react';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import { Connect } from './pages/Connect';
 import { Home } from './pages/Home';
 
 export const App: FC = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/connect" element={<Connect />} />
         </Routes>
     );
 };
--- packages/example/dapp/src/pages/Connect.tsx
+++ packages/example/dapp/src/pages/Connect.tsx
@@ -2,11 +2,11 @@ import type { FC } from 'react';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export const Home: FC = () => {
+export const Connect: FC = () => {
     return (
         <div>
-            <h1>dApp Example</h1>
-            <Link to="/connect">Connect Wallet</Link>
+            <h1>Connect Wallet</h1>
+            <Link to="/">Back</Link>
         </div>
     );
 };
