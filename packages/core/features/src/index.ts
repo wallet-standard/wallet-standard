6394f12b086b483c6668d7a84b9ca891746e2000
Jordan Sexton <jordan@jordansexton.com>
2022-09-07 19:08:35 -0500

    add feature helper types

--- packages/core/features/src/index.ts
+++ packages/core/features/src/index.ts
@@ -1,3 +1,22 @@
+import type { IdentifierRecord, Wallet } from '@wallet-standard/standard';
+import type { DecryptFeature } from './decrypt.js';
+import type { EncryptFeature } from './encrypt.js';
+import type { SignAndSendTransactionFeature } from './signAndSendTransaction.js';
+import type { SignMessageFeature } from './signMessage.js';
+import type { SignTransactionFeature } from './signTransaction.js';
+import type { SolanaSignAndSendTransactionFeature } from './solanaSignAndSendTransaction.js';
+
+export type WalletWithFeatures<Features extends IdentifierRecord<unknown>> = Wallet & { features: Features };
+
+export type WalletWithStandardFeatures = WalletWithFeatures<
+    | DecryptFeature
+    | EncryptFeature
+    | SignAndSendTransactionFeature
+    | SignMessageFeature
+    | SignTransactionFeature
+    | SolanaSignAndSendTransactionFeature
+>;
+
 export * from './decrypt.js';
 export * from './encrypt.js';
 export * from './signAndSendTransaction.js';
