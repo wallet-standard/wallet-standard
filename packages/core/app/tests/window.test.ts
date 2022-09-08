92a0868d88aabfe5f4427701e2946c8e909cea91
Jordan Sexton <jordan@jordansexton.com>
2022-09-07 19:08:11 -0500

    fix app package tests

--- packages/core/app/tests/window.test.ts
+++ packages/core/app/tests/window.test.ts
@@ -1,93 +1,82 @@
-import type { Wallet, WalletAccount, WalletEvents, WalletsWindow } from '@wallet-standard/standard';
+import type {
+    ConnectInput,
+    ConnectOutput,
+    Wallet,
+    WalletAccount,
+    WalletAccountEventNames,
+    WalletAccountEvents,
+    WalletEventNames,
+    WalletEvents,
+    WalletsWindow,
+} from '@wallet-standard/standard';
 import { initialize } from '..';
 
-interface SolanaWalletAccount extends WalletAccount {
-    chain: 'solana:mainnet';
-    features: never;
-    extensions: { one: 1; two: 2 };
-}
+type FooSignTransactionFeature = {
+    'foo:signTransaction': {
+        signTransaction(): void;
+    };
+};
 
-// A wallet that supports multiple account types
-type SolanaWallet = Wallet<SolanaWalletAccount>;
+type FooSignMessageFeature = {
+    'foo:signMessage': {
+        signMessage(): void;
+    };
+};
 
 class FooWalletAccount implements WalletAccount {
-    address = new Uint8Array();
+    address = '';
     publicKey = new Uint8Array();
-    chain = 'mainnet' as const;
-    features = {
-        signTransaction: {} as any,
-        signMessage: {} as any,
-    };
-    extensions = {};
+    chains = ['foo:mainnet'] as const;
+    features: ReadonlyArray<keyof FooSignTransactionFeature | keyof FooSignMessageFeature> = [
+        'foo:signTransaction',
+        'foo:signMessage',
+    ] as const;
+    on<E extends WalletAccountEventNames>(event: E, listener: WalletAccountEvents[E]): () => void {
+        throw new Error('Method not implemented.');
+    }
 }
 
-class FooWallet implements Wallet<FooWalletAccount> {
+class FooWallet implements Wallet {
     version = '1.0.0' as const;
     name = 'Foo';
-    icon = 'image';
-    chains = ['mainnet'] as const;
-    features = ['signTransaction', 'signMessage'] as const;
-    extensions = [] as const;
+    icon = `data:image/png;base64,` as const;
+    chains = ['foo:mainnet'] as const;
+    features: FooSignTransactionFeature & FooSignMessageFeature = {
+        'foo:signTransaction': {
+            // eslint-disable-next-line @typescript-eslint/no-empty-function
+            signTransaction() {},
+        },
+        'foo:signMessage': {
+            // eslint-disable-next-line @typescript-eslint/no-empty-function
+            signMessage() {},
+        },
+    };
     accounts = [new FooWalletAccount()];
-    hasMoreAccounts = false;
-    connect(...args: Parameters<Wallet<FooWalletAccount>['connect']>): ReturnType<Wallet<FooWalletAccount>['connect']> {
+    async connect(input: ConnectInput): Promise<ConnectOutput> {
         throw new Error('Method not implemented.');
     }
-    on<E extends keyof WalletEvents<FooWalletAccount>>(
-        event: E,
-        listener: WalletEvents<FooWalletAccount>[E]
-    ): () => void {
+    on<E extends WalletEventNames>(event: E, listener: WalletEvents[E]): () => void {
         throw new Error('Method not implemented.');
     }
 }
 
-const fooWallet1 = new FooWallet();
-const fooWallet2: Wallet<FooWalletAccount> = fooWallet1;
-
-fooWallet1.connect({
-    chains: ['mainnet'],
-    features: ['signTransaction', 'signMessage'],
-});
-
-fooWallet2.connect({
-    chains: ['mainnet'],
-    features: ['signTransaction', 'signMessage'],
-});
+declare const window: WalletsWindow;
 
 (async function () {
-    ((window as WalletsWindow<SolanaWalletAccount>).navigator.wallets ||= []).push({
+    (window.navigator.wallets ||= []).push({
         method: 'register',
-        wallets: [{} as SolanaWallet],
-        callback() {},
-    });
-    ((window as WalletsWindow<FooWalletAccount>).navigator.wallets ||= []).push({
-        method: 'register',
-        wallets: [fooWallet1],
-        callback() {},
-    });
-    ((window as WalletsWindow<FooWalletAccount>).navigator.wallets ||= []).push({
-        method: 'register',
-        wallets: [fooWallet2],
+        wallets: [new FooWallet()],
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
         callback() {},
     });
 
-    const wallets = initialize<SolanaWalletAccount | FooWalletAccount>();
+    const wallets = initialize();
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const wallet = wallets.get()[0]!;
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const account = (await wallet.connect({ chains: ['solana:mainnet'] })).accounts[0]!;
+    const account = (await wallet.connect({ chains: ['foo:mainnet'] })).accounts[0]!;
 
-    // FIXME: @ts-expect-error expected
-    account.features.signTransaction;
-    // @ts-expect-error expected
-    account.features.one;
-    // @ts-expect-error expected
-    account.features.two;
-    // @ts-expect-error expected
-    account.features.three;
-
-    // FIXME: passing the chain here should eliminate FooWallet
-    if ('signTransaction' in account.features) {
-        account.features.signTransaction.signTransaction();
+    if ('foo:signTransaction' in wallet.features) {
+        (wallet as Wallet & { features: FooSignTransactionFeature }).features['foo:signTransaction'].signTransaction();
     }
 })();
