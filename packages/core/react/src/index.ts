b976a3025f87750877a2c2716e506874145c19b3
Jordan Sexton <jordan@jordansexton.com>
2022-09-30 19:18:33 -0500

    add react account context

--- packages/core/react/src/index.ts
+++ packages/core/react/src/index.ts
@@ -1,6 +1,8 @@
 export * from './features/index.js';
 
 export * from './useWallet.js';
+export * from './useWalletAccount.js';
 export * from './useWallets.js';
+export * from './WalletAccountProvider.js';
 export * from './WalletProvider.js';
 export * from './WalletsProvider.js';
---
+++ packages/core/react/src/useWalletAccount.ts
@@ -0,0 +1,25 @@
+import type { WalletAccount } from '@wallet-standard/standard';
+import { createContext, useContext } from 'react';
+import { createDefaultContext } from './context.js';
+
+/** TODO: docs */
+export interface WalletAccountContextState {
+    walletAccount: WalletAccount | null;
+    setWalletAccount(wallet: WalletAccount | null): void;
+}
+
+const DEFAULT_WALLET_ACCOUNT_STATE: Readonly<WalletAccountContextState> = {
+    walletAccount: null,
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    setWalletAccount() {},
+} as const;
+
+const DEFAULT_WALLET_ACCOUNT_CONTEXT = createDefaultContext('WalletAccount', DEFAULT_WALLET_ACCOUNT_STATE);
+
+/** TODO: docs */
+export const WalletAccountContext = createContext(DEFAULT_WALLET_ACCOUNT_CONTEXT);
+
+/** TODO: docs */
+export function useWalletAccount(): WalletAccountContextState {
+    return useContext(WalletAccountContext);
+}
