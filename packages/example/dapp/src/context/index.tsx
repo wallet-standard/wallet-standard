7385dc59d3c7fa4c27fda8ac88f2db5008fe6526
Jordan Sexton <jordan@jordansexton.com>
2022-09-30 20:14:01 -0500

    use wallet standard provider

--- packages/example/dapp/src/context/index.tsx
+++ packages/example/dapp/src/context/index.tsx
@@ -1,13 +1,7 @@
-import { ConnectProvider, WalletProvider, WalletsProvider } from '@wallet-standard/react';
+import { WalletStandardProvider } from '@wallet-standard/react';
 import type { FC, ReactNode } from 'react';
 import React from 'react';
 
 export const AppContext: FC<{ children: NonNullable<ReactNode> }> = ({ children }) => {
-    return (
-        <WalletsProvider>
-            <WalletProvider>
-                <ConnectProvider>{children}</ConnectProvider>
-            </WalletProvider>
-        </WalletsProvider>
-    );
+    return <WalletStandardProvider>{children}</WalletStandardProvider>;
 };
