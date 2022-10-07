25e8960ac4c3988e0b267bf8cba7a30103095e10
Jordan Sexton <jordan@jordansexton.com>
2022-10-07 18:38:35 -0500

    add docs for solana chains

--- packages/solana/chains/src/index.ts
+++ packages/solana/chains/src/index.ts
@@ -1,16 +1,16 @@
-/** Solana Mainnet (beta), e.g. https://api.mainnet-beta.solana.com */
+/** Solana Mainnet (beta) cluster, e.g. https://api.mainnet-beta.solana.com */
 export const SOLANA_MAINNET_CHAIN = 'solana:mainnet';
 
-/** Solana Devnet, e.g. https://api.devnet.solana.com */
+/** Solana Devnet cluster, e.g. https://api.devnet.solana.com */
 export const SOLANA_DEVNET_CHAIN = 'solana:devnet';
 
-/** Solana Testnet, e.g. https://api.testnet.solana.com */
+/** Solana Testnet cluster, e.g. https://api.testnet.solana.com */
 export const SOLANA_TESTNET_CHAIN = 'solana:testnet';
 
-/** Solana Localnet, e.g. http://localhost:8899 */
+/** Solana Localnet cluster, e.g. http://localhost:8899 */
 export const SOLANA_LOCALNET_CHAIN = 'solana:localnet';
 
-/** TODO: docs */
+/** Array of all Solana clusters */
 export const SOLANA_CHAINS = [
     SOLANA_MAINNET_CHAIN,
     SOLANA_DEVNET_CHAIN,
@@ -18,9 +18,5 @@ export const SOLANA_CHAINS = [
     SOLANA_LOCALNET_CHAIN,
 ] as const;
 
-/** TODO: docs */
-export type SolanaChain =
-    | typeof SOLANA_MAINNET_CHAIN
-    | typeof SOLANA_DEVNET_CHAIN
-    | typeof SOLANA_TESTNET_CHAIN
-    | typeof SOLANA_LOCALNET_CHAIN;
+/** Type of all Solana clusters */
+export type SolanaChain = typeof SOLANA_CHAINS[number];
