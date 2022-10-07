3a1a9d28b76c82d51640292a1a4665e7fcfbdd86
Jordan Sexton <jordan@jordansexton.com>
2022-10-07 16:01:04 -0500

    move events to feature

--- packages/core/features/src/index.ts
+++ packages/core/features/src/index.ts
@@ -2,6 +2,7 @@ import type { WalletWithFeatures } from '@wallet-standard/standard';
 import type { ConnectFeature } from './connect.js';
 import type { DecryptFeature } from './decrypt.js';
 import type { EncryptFeature } from './encrypt.js';
+import type { EventsFeature } from './events.js';
 import type { SignAndSendTransactionFeature } from './signAndSendTransaction.js';
 import type { SignMessageFeature } from './signMessage.js';
 import type { SignTransactionFeature } from './signTransaction.js';
@@ -11,6 +12,7 @@ export type StandardFeatures =
     | ConnectFeature
     | DecryptFeature
     | EncryptFeature
+    | EventsFeature
     | SignAndSendTransactionFeature
     | SignMessageFeature
     | SignTransactionFeature;
@@ -21,6 +23,7 @@ export type WalletWithStandardFeatures = WalletWithFeatures<StandardFeatures>;
 export * from './connect.js';
 export * from './decrypt.js';
 export * from './encrypt.js';
+export * from './events.js';
 export * from './signAndSendTransaction.js';
 export * from './signMessage.js';
 export * from './signTransaction.js';
