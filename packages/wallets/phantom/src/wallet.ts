10e4fe2d0a0512493f3ba8a619170b18bb522ed7
Jordan Sexton <jordan@jordansexton.com>
2022-10-09 22:55:05 -0500

    add phantom wallet

---
+++ packages/wallets/phantom/src/wallet.ts
@@ -0,0 +1,265 @@
+import { Transaction } from '@solana/web3.js';
+import type {
+    ConnectFeature,
+    ConnectMethod,
+    EventsFeature,
+    EventsListeners,
+    EventsNames,
+    EventsOnMethod,
+    SignMessageFeature,
+    SignMessageMethod,
+    SignMessageOutput,
+} from '@wallet-standard/features';
+import type {
+    SolanaSignAndSendTransactionFeature,
+    SolanaSignAndSendTransactionMethod,
+    SolanaSignAndSendTransactionOutput,
+    SolanaSignTransactionFeature,
+    SolanaSignTransactionMethod,
+    SolanaSignTransactionOutput,
+} from '@wallet-standard/solana-features';
+import type { Wallet } from '@wallet-standard/standard';
+import { decode } from 'bs58';
+import { icon } from './icon.js';
+import type { SolanaChain } from './solana.js';
+import { isSolanaChain, SOLANA_CHAINS } from './solana.js';
+import { bytesEqual, PhantomWalletAccount } from './util.js';
+import type { PhantomWindow, WindowPhantom } from './window.js';
+
+declare const window: PhantomWindow;
+
+export type PhantomFeature = {
+    'phantom:': {
+        phantom: WindowPhantom;
+    };
+};
+
+export class PhantomWallet implements Wallet {
+    readonly #listeners: { [E in EventsNames]?: EventsListeners[E][] } = {};
+    readonly #version = '1.0.0' as const;
+    readonly #name = 'Phantom' as const;
+    readonly #icon = icon;
+    #account: PhantomWalletAccount | null = null;
+
+    get version() {
+        return this.#version;
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    get icon() {
+        return this.#icon;
+    }
+
+    get chains() {
+        return SOLANA_CHAINS.slice();
+    }
+
+    get features(): ConnectFeature &
+        EventsFeature &
+        SolanaSignAndSendTransactionFeature &
+        SolanaSignTransactionFeature &
+        SignMessageFeature &
+        PhantomFeature {
+        return {
+            'standard:connect': {
+                version: '1.0.0',
+                connect: this.#connect,
+            },
+            'standard:events': {
+                version: '1.0.0',
+                on: this.#on,
+            },
+            'solana:signAndSendTransaction': {
+                version: '1.0.0',
+                supportedTransactionVersions: ['legacy'],
+                signAndSendTransaction: this.#signAndSendTransaction,
+            },
+            'solana:signTransaction': {
+                version: '1.0.0',
+                supportedTransactionVersions: ['legacy'],
+                signTransaction: this.#signTransaction,
+            },
+            'standard:signMessage': {
+                version: '1.0.0',
+                signMessage: this.#signMessage,
+            },
+            'phantom:': {
+                get phantom() {
+                    return window.phantom;
+                },
+            },
+        };
+    }
+
+    get accounts() {
+        return this.#account ? [this.#account] : [];
+    }
+
+    constructor() {
+        if (new.target === PhantomWallet) {
+            Object.freeze(this);
+        }
+
+        window.phantom.solana.on('connect', this.#connected, this);
+        window.phantom.solana.on('disconnect', this.#disconnected, this);
+        window.phantom.solana.on('accountChanged', this.#reconnected, this);
+
+        this.#connected();
+    }
+
+    #on: EventsOnMethod = (event, listener) => {
+        this.#listeners[event]?.push(listener) || (this.#listeners[event] = [listener]);
+        return (): void => this.#off(event, listener);
+    };
+
+    #emit<E extends EventsNames>(event: E, ...args: Parameters<EventsListeners[E]>): void {
+        // eslint-disable-next-line prefer-spread
+        this.#listeners[event]?.forEach((listener) => listener.apply(null, args));
+    }
+
+    #off<E extends EventsNames>(event: E, listener: EventsListeners[E]): void {
+        this.#listeners[event] = this.#listeners[event]?.filter((existingListener) => listener !== existingListener);
+    }
+
+    #connected = () => {
+        const address = window.phantom.solana.publicKey?.toBase58();
+        if (address) {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            const publicKey = window.phantom.solana.publicKey!.toBytes();
+
+            const account = this.#account;
+            if (!account || account.address !== address || !bytesEqual(account.publicKey, publicKey)) {
+                this.#account = new PhantomWalletAccount({ address, publicKey });
+                this.#emit('change', { accounts: this.accounts });
+            }
+        }
+    };
+
+    #disconnected = () => {
+        if (this.#account) {
+            this.#account = null;
+            this.#emit('change', { accounts: this.accounts });
+        }
+    };
+
+    #reconnected = () => {
+        if (window.phantom.solana.publicKey) {
+            this.#connected();
+        } else {
+            this.#disconnected();
+        }
+    };
+
+    #connect: ConnectMethod = async ({ silent } = {}) => {
+        await window.phantom.solana.connect(silent ? { onlyIfTrusted: true } : undefined);
+
+        this.#connected();
+
+        return { accounts: this.accounts };
+    };
+
+    #signAndSendTransaction: SolanaSignAndSendTransactionMethod = async (...inputs) => {
+        if (!this.#account) throw new Error('not connected');
+
+        const outputs: SolanaSignAndSendTransactionOutput[] = [];
+
+        if (inputs.length === 1) {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            const { transaction, account, chain, options } = inputs[0]!;
+            if (account !== this.#account) throw new Error('invalid account');
+            if (!isSolanaChain(chain)) throw new Error('invalid chain');
+
+            const { signature } = await window.phantom.solana.signAndSendTransaction(
+                Transaction.from(transaction),
+                options
+            );
+
+            outputs.push({ signature: decode(signature) });
+        } else if (inputs.length > 1) {
+            for (const input of inputs) {
+                outputs.push(...(await this.#signAndSendTransaction(input)));
+            }
+        }
+
+        return outputs;
+    };
+
+    #signTransaction: SolanaSignTransactionMethod = async (...inputs) => {
+        if (!this.#account) throw new Error('not connected');
+
+        const outputs: SolanaSignTransactionOutput[] = [];
+
+        if (inputs.length === 1) {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            const { transaction, account, chain } = inputs[0]!;
+            if (account !== this.#account) throw new Error('invalid account');
+            if (chain && !isSolanaChain(chain)) throw new Error('invalid chain');
+
+            const signedTransaction = await window.phantom.solana.signTransaction(Transaction.from(transaction));
+
+            outputs.push({
+                signedTransaction: new Uint8Array(
+                    signedTransaction.serialize({
+                        requireAllSignatures: false,
+                        verifySignatures: false,
+                    })
+                ),
+            });
+        } else if (inputs.length > 1) {
+            let chain: SolanaChain | undefined = undefined;
+            for (const input of inputs) {
+                if (input.account !== this.#account) throw new Error('invalid account');
+                if (input.chain) {
+                    if (!isSolanaChain(input.chain)) throw new Error('invalid chain');
+                    if (chain) {
+                        if (input.chain !== chain) throw new Error('conflicting chain');
+                    } else {
+                        chain = input.chain;
+                    }
+                }
+            }
+
+            const transactions = inputs.map(({ transaction }) => Transaction.from(transaction));
+
+            const signedTransactions = await window.phantom.solana.signAllTransactions(transactions);
+
+            outputs.push(
+                ...signedTransactions.map((signedTransaction) => ({
+                    signedTransaction: new Uint8Array(
+                        signedTransaction.serialize({
+                            requireAllSignatures: false,
+                            verifySignatures: false,
+                        })
+                    ),
+                }))
+            );
+        }
+
+        return outputs;
+    };
+
+    #signMessage: SignMessageMethod = async (...inputs) => {
+        if (!this.#account) throw new Error('not connected');
+
+        const outputs: SignMessageOutput[] = [];
+
+        if (inputs.length === 1) {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            const { message, account } = inputs[0]!;
+            if (account !== this.#account) throw new Error('invalid account');
+
+            const { signature } = await window.phantom.solana.signMessage(message);
+
+            outputs.push({ signedMessage: message, signature });
+        } else if (inputs.length > 1) {
+            for (const input of inputs) {
+                outputs.push(...(await this.#signMessage(input)));
+            }
+        }
+
+        return outputs;
+    };
+}
