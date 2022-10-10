42133effd93ab0580528802522fb59efdeb7cdd9
Jordan Sexton <jordan@jordansexton.com>
2022-10-10 17:08:25 -0500

    add example with registered wallets

---
+++ packages/example/solana-wallet-adapter-example/src/components/ContextProvider.tsx
@@ -0,0 +1,107 @@
+import { createTheme, StyledEngineProvider, ThemeProvider } from '@mui/material';
+import { deepPurple, pink } from '@mui/material/colors';
+import { WalletModalProvider as AntDesignWalletModalProvider } from '@solana/wallet-adapter-ant-design';
+import type { WalletError } from '@solana/wallet-adapter-base';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { WalletDialogProvider as MaterialUIWalletDialogProvider } from '@solana/wallet-adapter-material-ui';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider as ReactUIWalletModalProvider } from '@solana/wallet-adapter-react-ui';
+// import { PhantomWalletAdapter, GlowWalletAdapter, BackpackWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { register as registerGlow } from '@wallet-standard/wallets-glow';
+import { register as registerBackpack } from '@wallet-standard/wallets-backpack';
+import { register as registerPhantom } from '@wallet-standard/wallets-phantom';
+import { clusterApiUrl } from '@solana/web3.js';
+import { useStandardWalletAdapters } from '@wallet-standard/solana';
+import { SnackbarProvider, useSnackbar } from 'notistack';
+import type { FC, ReactNode } from 'react';
+import React, { useCallback, useEffect, useMemo } from 'react';
+import { AutoConnectProvider, useAutoConnect } from './AutoConnectProvider';
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: deepPurple[700],
+        },
+        secondary: {
+            main: pink[700],
+        },
+    },
+    components: {
+        MuiButtonBase: {
+            styleOverrides: {
+                root: {
+                    justifyContent: 'flex-start',
+                },
+            },
+        },
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    textTransform: 'none',
+                    padding: '12px 16px',
+                },
+                startIcon: {
+                    marginRight: 8,
+                },
+                endIcon: {
+                    marginLeft: 8,
+                },
+            },
+        },
+    },
+});
+
+const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+    const { autoConnect } = useAutoConnect();
+
+    // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
+    const network = WalletAdapterNetwork.Devnet;
+
+    // You can also provide a custom RPC endpoint
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+    const wallets = useMemo(() => [], []);
+    const adapters = useStandardWalletAdapters([]);
+
+    useEffect(() => {
+        registerBackpack();
+        registerGlow();
+        registerPhantom();
+    }, []);
+
+    const { enqueueSnackbar } = useSnackbar();
+    const onError = useCallback(
+        (error: WalletError) => {
+            enqueueSnackbar(error.message ? `${error.name}: ${error.message}` : error.name, { variant: 'error' });
+            console.error(error);
+        },
+        [enqueueSnackbar]
+    );
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={adapters} onError={onError} autoConnect={autoConnect}>
+                <MaterialUIWalletDialogProvider>
+                    <AntDesignWalletModalProvider>
+                        <ReactUIWalletModalProvider>{children}</ReactUIWalletModalProvider>
+                    </AntDesignWalletModalProvider>
+                </MaterialUIWalletDialogProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
+
+export const ContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+    return (
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={theme}>
+                <SnackbarProvider>
+                    <AutoConnectProvider>
+                        <WalletContextProvider>{children}</WalletContextProvider>
+                    </AutoConnectProvider>
+                </SnackbarProvider>
+            </ThemeProvider>
+        </StyledEngineProvider>
+    );
+};
