e1e964bd7b4049e3869dbad4d6dd84fd6d22b0e7
Jordan Sexton <jordan@jordansexton.com>
2022-10-09 22:38:18 -0500

    add arraysEqual method

--- packages/core/util/src/util.ts
+++ packages/core/util/src/util.ts
@@ -1,10 +1,24 @@
 /** TODO: docs */
 export function bytesEqual(a: Uint8Array, b: Uint8Array): boolean {
+    return arraysEqual(a, b);
+}
+
+interface Indexed<T> {
+    length: number;
+    [index: number]: T;
+}
+
+/** TODO: docs */
+export function arraysEqual<T>(a: Indexed<T>, b: Indexed<T>): boolean {
+    if (a === b) return true;
+
     const length = a.length;
     if (length !== b.length) return false;
+
     for (let i = 0; i < length; i++) {
         if (a[i] !== b[i]) return false;
     }
+
     return true;
 }
 
