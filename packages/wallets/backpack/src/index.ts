cd2cdcab3b0b8225a3384796bb4de609c45f045d
Jordan Sexton <jordan@jordansexton.com>
2022-10-10 16:51:55 -0500

    move register function to file

--- packages/wallets/backpack/src/index.ts
+++ packages/wallets/backpack/src/index.ts
@@ -1,8 +1,1 @@
-import type { WalletsWindow } from '@wallet-standard/standard';
-import { BackpackWallet } from './wallet.js';
-
-declare const window: WalletsWindow;
-
-export function register(): void {
-    (window.navigator.wallets ||= []).push(({ register }) => register(new BackpackWallet()));
-}
+export * from './register.js';
--- packages/wallets/glow/src/index.ts
+++ packages/wallets/glow/src/index.ts
@@ -1,8 +1,1 @@
-import type { WalletsWindow } from '@wallet-standard/standard';
-import { GlowWallet } from './wallet.js';
-
-declare const window: WalletsWindow;
-
-export function register(): void {
-    (window.navigator.wallets ||= []).push(({ register }) => register(new GlowWallet()));
-}
+export * from './register.js';
