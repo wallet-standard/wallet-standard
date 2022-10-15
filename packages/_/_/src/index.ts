ec2d4860f7bee450c65c20136dadbcbba0c90a6e
Jordan Sexton <jordan@jordansexton.com>
2022-10-15 18:53:12 -0500

    more rc.0

--- packages/_/_/src/index.ts
+++ packages/_/_/src/index.ts
@@ -1,1 +1,4 @@
-export {};
+export * from '@wallet-standard/core';
+export * from '@wallet-standard/ethereum';
+export * from '@wallet-standard/experimental';
+export * from '@wallet-standard/react';
