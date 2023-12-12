6a969b51b9515df42260c731214371214779d20d
Steven Luscher <steveluscher@users.noreply.github.com>
2023-12-12 11:41:32 -0800

    Reimplement the `useWallets()` hook with `useSyncExternalStore` (#92)
    * `get()` now returns the same array object unless the wallets have changed
    
    * Create fifty-crews-boil.md
    
    * `get()` now returns the same array object unless the wallets have changed
    
    * Reimplement the `useWallets()` hook with `useSyncExternalStore`
    
    * Create late-gorillas-run.md

--- packages/react/core/src/useWallets.ts
+++ packages/react/core/src/useWallets.ts
@@ -1,31 +1,25 @@
 import { getWallets } from '@wallet-standard/app';
 import type { Wallet } from '@wallet-standard/base';
-import { useMemo, useState, useEffect } from 'react';
+import { useCallback, useSyncExternalStore } from 'react';
+import { useStable } from './useStable.js';
+
+function getServerSnapshot(): readonly Wallet[] {
+    return [];
+}
 
 /** TODO: docs */
 export function useWallets(): readonly Wallet[] {
-    // Initialize `window.navigator.wallets` and obtain a synchronous API.
-    const { get, on } = useMemo(() => getWallets(), []);
-
-    // Synchronously get the wallets that have registered already so that they can be accessed on the first render.
-    const [wallets, setWallets] = useState(() => get());
-
-    useEffect(() => {
-        const destructors: (() => void)[] = [];
-
-        // TODO: figure out if this can ever actually happen
-        // FIXME: this can definitely happen, refactor similar to @solana/wallet-standard-wallet-adapter-react
-        // Get and set the wallets that have been registered already, in case they changed since the state initializer.
-        setWallets(get());
-
-        // Add an event listener to add any wallets that are registered after this point.
-        destructors.push(on('register', () => setWallets(get())));
-
-        // Add an event listener to remove any wallets that are unregistered after this point.
-        destructors.push(on('unregister', () => setWallets(get())));
-
-        return () => destructors.forEach((destroy) => destroy());
-    }, [get, on]);
-
-    return wallets;
+    const { get: getSnapshot, on } = useStable(getWallets);
+    const subscribe = useCallback(
+        (callback: () => void) => {
+            const disposeRegisterListener = on('register', callback);
+            const disposeUnregisterListener = on('unregister', callback);
+            return () => {
+                disposeRegisterListener();
+                disposeUnregisterListener();
+            };
+        },
+        [on]
+    );
+    return useSyncExternalStore<readonly Wallet[]>(subscribe, getSnapshot, getServerSnapshot);
 }
