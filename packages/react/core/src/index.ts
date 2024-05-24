e61f5dc8e3a157494f60e228c5d322128d15d835
steveluscher <steven.luscher@anza.xyz>
2024-05-24 07:27:30 +0000

    Delete the remainder of `@wallet-standard/react` that is no longer used (anza-xyz/wallet-standard/pull/59)

--- packages/react/core/src/index.ts
+++ packages/react/core/src/index.ts
@@ -2,9 +2,4 @@ export * from '@wallet-standard/ui';
 
 export * from './features/index.js';
 
-export * from './useWallet.js';
-export * from './useWalletAccount.js';
 export * from './useWallets.js';
-export * from './WalletAccountProvider.js';
-export * from './WalletProvider.js';
-export * from './WalletStandardProvider.js';
