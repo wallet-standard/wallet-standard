4125be217ca30e7b8d9f42a51e1187cbff4d8059
steveluscher <steven.luscher@anza.xyz>
2024-05-28 22:01:22 +0000

    Delete the dApp example (anza-xyz/wallet-standard/pull/62)

--- packages/example/dapp/src/context/index.tsx
+++
@@ -1,7 +0,0 @@
-import { WalletStandardProvider } from '@wallet-standard/react';
-import type { FC, ReactNode } from 'react';
-import React from 'react';
-
-export const AppContext: FC<{ children: NonNullable<ReactNode> }> = ({ children }) => {
-    return <WalletStandardProvider>{children}</WalletStandardProvider>;
-};
