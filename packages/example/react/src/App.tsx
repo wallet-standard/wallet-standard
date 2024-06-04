5d00618ef43625e5ae521b63226df3b9942685c8
steveluscher <steven.luscher@anza.xyz>
2024-06-04 19:14:55 +0000

    A `useWallets()` hook you can use to materialize `UiWallet` objects for all of the connected wallets (anza-xyz/wallet-standard/pull/53)

--- packages/example/react/src/App.tsx
+++ packages/example/react/src/App.tsx
@@ -1,26 +1,17 @@
 import { GlowWalletAdapter } from '@solana/wallet-adapter-glow';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
 import { registerWalletAdapter, SOLANA_MAINNET_CHAIN } from '@solana/wallet-standard';
-import { useWallets, WalletProvider } from '@wallet-standard/react';
-import type { FC, ReactNode } from 'react';
+import { useWallets } from '@wallet-standard/react';
+import type { FC } from 'react';
 import React, { useEffect } from 'react';
 
 export const App: FC = () => {
-    return (
-        <Context>
-            <Content />
-        </Context>
-    );
-};
-
-const Context: FC<{ children: NonNullable<ReactNode> }> = ({ children }) => {
     useEffect(() => {
         const adapters = [new PhantomWalletAdapter(), new GlowWalletAdapter()];
         const destructors = adapters.map((adapter) => registerWalletAdapter(adapter, SOLANA_MAINNET_CHAIN));
         return () => destructors.forEach((destroy) => destroy());
     }, []);
-
-    return <WalletProvider>{children}</WalletProvider>;
+    return <Content />;
 };
 
 const Content: FC = () => {
